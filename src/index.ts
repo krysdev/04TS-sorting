0dee23ee1b8de1c498558ecc9aff76cd771b6e44
KrysDev <webdevkrys@gmail.com>
2023-05-12 20:39:50 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+class Sorter {
+  // these 4 lines are equivalent of the 1 below (type + initialize)
+  // collection: number[];
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
