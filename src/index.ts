9de4d989136b9399176feb8c35169454cab35802
KrysDev <webdevkrys@gmail.com>
2023-05-13 19:31:45 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,25 @@ class Sorter {
 
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    // sorting algorithm (bubble sort)
+
+    // array of numbers (not for strings)
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // compare the elements
+        if (this.collection[j] > this.collection[j + 1]) {
+          // swap the elements
+          const leftElement = this.collection[j];
+          this.collection[j] = this.collection[j + 1]; // take the right element and move it to the left side
+          this.collection[j + 1] = leftElement; // take the left el and move it to the left
+        }
+      }
+    }
+    //
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
