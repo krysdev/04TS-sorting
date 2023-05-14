0b45fc6c3cdd0cc86e8f9fe7d1889fce39cb46dd
KrysDev <webdevkrys@gmail.com>
2023-05-14 13:04:11 +0100

    x

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,27 @@
+// 2. only for strings
+
+export class CharactersCollection {
+  // one line
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    // you can't swap characters directly in the string, so there will be an array of characters
+    const characters = this.data.split('');
+
+    const leftHand = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftHand;
+
+    this.data = characters.join('');
+  }
+}
