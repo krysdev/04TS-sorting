8d7db25fdb087e560bb7182684294ac2288139a3
KrysDev <webdevkrys@gmail.com>
2023-05-14 13:05:17 +0100

    x

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -14,8 +14,8 @@ export class Sorter {
     // general sorting algorithm (bubble sort)
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.collection.compare(j, j + 1)) { // compare elements
+          this.collection.swap(j, j + 1);  // swap the elements if needed
         }
       }
     }
